106e5ae521e4b0693ef9e4caa07dfdc4ccf2b619
Tadej Novak <tadej@tano.si>
2025-11-22 14:56:44 +0100

    Fix daylight savings time detection

--- src/exif_maker_notes/fixes/timezone.py
+++ src/exif_maker_notes/fixes/timezone.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from exif_maker_notes.cli.config import strtobool
 from exif_maker_notes.fixes.fix import Fix
 
 if TYPE_CHECKING:
@@ -27,6 +28,17 @@ class TimezoneFix(Fix):
         maker_note_timezone = metadata.get("MakerNotes:TimeZone")
         if not maker_note_timezone:
             return {}
+
+        maker_note_dst = strtobool(metadata.get("MakerNotes:DaylightSavings", "0"))
+        if maker_note_dst:
+            if maker_note_timezone == "Z":
+                maker_note_timezone = "+01:00"
+            elif maker_note_timezone.startswith("+"):
+                hours = int(maker_note_timezone[1:3]) + 1
+                maker_note_timezone = f"+{hours:02d}{maker_note_timezone[3:]}"
+            elif maker_note_timezone.startswith("-"):
+                hours = int(maker_note_timezone[1:3]) - 1
+                maker_note_timezone = f"-{hours:02d}{maker_note_timezone[3:]}"
 
         if self.logger:
             self.logger.info(
